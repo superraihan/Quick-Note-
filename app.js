038bf5901e974be380a9634b81ffe76d0ac1c544
MD. RAIHAN KABIR NIBIR <124171192+superraihan@users.noreply.github.com>
2025-08-28 21:20:31 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let notes = []
+function openNoteDialog() {
+const dialog = document.getElementById('noteDialog');
+const textarea = document.getElementById('noteTitle');
+const contentInput = document.getElementById('noteContent');
+
+dialog.showModal()
+titleInput.focus()
+}
